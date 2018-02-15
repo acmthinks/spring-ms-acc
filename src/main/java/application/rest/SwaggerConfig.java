aafeb0d67791e2cca1d36a319010f67bc1b04049
acm1 <acm@us.ibm.com>
2018-02-15 14:10:57 -0500

    using "any" path selectors

--- src/main/java/application/rest/SwaggerConfig.java
+++ src/main/java/application/rest/SwaggerConfig.java
@@ -3,11 +3,11 @@ package application.rest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
 @EnableSwagger2
@@ -15,20 +15,12 @@ import static springfox.documentation.builders.PathSelectors.regex;
 public class SwaggerConfig {
     @Bean
 
-    public Docket v1Api() {
+    public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("application.rest.v1"))
-                .paths(regex("/v1.*"))
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
                 .build();
     }
     
-    @Bean
-    public Docket v2Api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("application.rest.v2"))
-                .paths(regex("/v2.*"))
-                .build();
-    }
 }

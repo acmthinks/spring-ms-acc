bf3bbc16f61adfae435b33ef8b190417ea8ea55d
acm1 <acm@us.ibm.com>
2018-02-15 13:46:01 -0500

    futzing with swagger config

--- src/main/java/application/rest/SwaggerConfig.java
+++ src/main/java/application/rest/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package application.rest.v2;
+package application.rest;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,9 +12,17 @@ import static springfox.documentation.builders.PathSelectors.regex;
 @Configuration
 @EnableSwagger2
 
-public class SwaggerConfig2 {
+public class SwaggerConfig {
     @Bean
 
+    public Docket v1Api() {
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("application.rest.v1"))
+                .paths(regex("/v1.*"))
+                .build();
+    }
     public Docket v2Api() {
 
         return new Docket(DocumentationType.SWAGGER_2)
